eaa4aabb31c4d59af732efcdf881206c3f92bcf1
tetsuo <ogunduz@gmail.com>
2015-06-10 04:00:19 +0300

    test extending template

---
+++ test/extend-tpl.js
@@ -0,0 +1,23 @@
+var compile = require('..');
+var BaseTemplate = require('../template');
+var test = require('tap').test;
+var inherits = require('util').inherits;
+
+inherits(Template, BaseTemplate);
+
+function Template () {}
+
+Template.prototype.e = function (s) { return 'qux' };
+
+test('extend template', function (t) {
+  var s = compile();
+  s.on('data', function (render) {
+    var actual = render.call(null, { s: "555" }, require('./types'), Template);
+    t.deepEqual(actual, {
+      tag: "x", attrs: {},
+      children: [ { text: "qux" }, { text: "qux" } ]
+    });
+    t.end();
+  });
+  s.end('<x>y{s}</x>')
+});
